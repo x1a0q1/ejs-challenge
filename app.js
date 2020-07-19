b008c0ac577454cee3c4db658bfc6ec08aefda47
x1a09i <x1a09i.dev@gmail.com>
2020-07-19 15:38:22 +0900

    Challenge-1 render home.ejs

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
