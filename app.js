05ad72d9127897665092b3e0fc77bdc12e9a3ab6
x1a09i <x1a09i.dev@gmail.com>
2020-07-23 14:58:17 +0900

    Challenge-18 use lodash to deal with the route parameters

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -26,10 +27,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/posts/:title", (req, res) => {
-  console.log(req.params.title);
-
   posts.forEach((post) => {
-    if (post.title === req.params.title) {
+    if (_.lowerCase(post.title) === _.lowerCase(req.params.title)) {
       console.log("Matched!");
     }
   });
